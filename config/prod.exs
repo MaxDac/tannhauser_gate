343db37fcd21607b3855a1ad65de787aecd1ab5f
Massimiliano D Acunzo <massimiliano.dacunzo@engdb.com.br>
2020-08-12 13:30:39 -0300

    Versioning

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :tannhauser_gate_web, TannhauserGateWeb.Endpoint,
   http: [port: 8080],
-  url: [host: "tannhausergate.com", port: 8080],
+  url: [host: "tannhausergate.com", port: 80],
   debug_errors: true,
   server: true#,
 #  cache_static_manifest: "priv/static/manifest/cache_manifest.json"
